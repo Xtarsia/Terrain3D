9929d5ff47a61475e541e3aaa958fa23829a07e7
Xtarsia <69606701+Xtarsia@users.noreply.github.com>
2025-04-21 21:04:23 +0100

    implement weighted texture accumulation and flat normal toggle

--- src/shaders/dual_scaling.glsl
+++ src/shaders/dual_scaling.glsl
@@ -9,71 +9,35 @@ uniform float tri_scale_reduction : hint_range(0.001,1) = 0.3;
 uniform float dual_scale_far : hint_range(0,1000) = 170.0;
 uniform float dual_scale_near : hint_range(0,1000) = 100.0;
 
-//INSERT: DUAL_SCALING_BASE
-	// If dual scaling, apply to base texture.
-	if(region < 0) {
-		mat_scale *= tri_scale_reduction;
-	}
-	float far_factor = clamp(smoothstep(dual_scale_near, dual_scale_far, length(v_vertex - _camera_pos)), 0.0, 1.0);
-	if (out_mat.blend < 1.0 && (
-			(far_factor < 1. && (out_mat.base == dual_scale_texture)) || out_mat.base != dual_scale_texture)) {
-		// 2 lookups
-		//each time we change scale, recalculate antitiling from baseline to maintain continuity.
-		matUV = detiling(base_uv * mat_scale, index_pos * mat_scale, out_mat.base, normal_angle);
-		dd1.xy = rotate_plane(ddxy.xy, -normal_angle);
-		dd1.zw = rotate_plane(ddxy.zw, -normal_angle);
-		dd1 *= mat_scale;
-		albedo_ht = textureGrad(_texture_array_albedo, vec3(matUV, float(out_mat.base)), dd1.xy, dd1.zw);
-		normal_rg = textureGrad(_texture_array_normal, vec3(matUV, float(out_mat.base)), dd1.xy, dd1.zw);
-
-		// Unpack & rotate base normal for blending
-		normal_rg.xyz = unpack_normal(normal_rg);
-		normal_rg.xz = rotate_plane(normal_rg.xz, -normal_angle);
-	}
-
-	if (far_factor > 0. && ((out_mat.base == dual_scale_texture && out_mat.blend < 1.0)
-			|| (out_mat.over == dual_scale_texture && out_mat.blend > 0.0))) {
-		// 2 lookups
-		mat_scale *= dual_scale_reduction;
-		float dual_scale_normal = uv_rotation + p_angle; //do not add near & far rotations
-		// Do not apply detiling if tri-scale reduction occurs.
-		matUV = region < 0 ? base_uv * mat_scale : detiling(base_uv * mat_scale, index_pos * mat_scale, dual_scale_texture, dual_scale_normal);
-		dd1.xy = rotate_plane(ddxy.xy, -dual_scale_normal);
-		dd1.zw = rotate_plane(ddxy.zw, -dual_scale_normal);
-		dd1 *= mat_scale;
-		albedo_far = textureGrad(_texture_array_albedo, vec3(matUV, float(dual_scale_texture)), dd1.xy, dd1.zw);
-		normal_far = textureGrad(_texture_array_normal, vec3(matUV, float(dual_scale_texture)), dd1.xy, dd1.zw);
-
-		// Unpack & rotate dual scale normal for blending
-		normal_far.xyz = unpack_normal(normal_far);
-		normal_far.xz = rotate_plane(normal_far.xz, -dual_scale_normal);
-	}
-	
-	if (out_mat.base == dual_scale_texture) {
-		albedo_ht = mix(albedo_ht, albedo_far, far_factor);
-		normal_rg = mix(normal_rg, normal_far, far_factor);
-	}
-
-//INSERT: UNI_SCALING_BASE
-	if (out_mat.blend < 1.0) {
-		// 2 lookups
-		matUV = detiling(base_uv * mat_scale, index_pos * mat_scale, out_mat.base, normal_angle);
-		dd1.xy = rotate_plane(ddxy.xy, -normal_angle);
-		dd1.zw = rotate_plane(ddxy.zw, -normal_angle);
-		dd1 *= mat_scale;
-		albedo_ht = textureGrad(_texture_array_albedo, vec3(matUV, float(out_mat.base)), dd1.xy, dd1.zw);
-		normal_rg = textureGrad(_texture_array_normal, vec3(matUV, float(out_mat.base)), dd1.xy, dd1.zw);
-
-		// Unpack & rotate base normal for blending
-		normal_rg.xyz = unpack_normal(normal_rg).xyz;
-		normal_rg.xz = rotate_plane(normal_rg.xz, -normal_angle);
-	}
-
-//INSERT: DUAL_SCALING_OVERLAY
-		// If dual scaling, apply to overlay texture
-		if(out_mat.over == dual_scale_texture) {
-			albedo_ht2 = mix(albedo_ht2, albedo_far, far_factor);
-			normal_rg2 = mix(normal_rg2, normal_far, far_factor);
+//INSERT: DUAL_SCALING
+		// dual scaling
+		float far_factor = clamp(smoothstep(dual_scale_near, dual_scale_far, length(v_vertex - _camera_pos)), 0.0, 1.0);
+		vec4 far_alb = vec4(0.);
+		vec4 far_nrm = vec4(0.);
+		if (far_factor > 0. && (data.texture_id[0] == dual_scale_texture || data.texture_id[1] == dual_scale_texture)) {
+			float far_scale = _texture_uv_scale_array[dual_scale_texture] * dual_scale_reduction;
+			far_scale *= index.z < 0 ? tri_scale_reduction : 1.;
+			float far_angle = i_angle + p_angle;
+			vec2 far_uv = detiling(i_uv * far_scale, i_pos * far_scale, dual_scale_texture, far_angle);
+			mat2 far_align = rotate_plane(-far_angle);
+			mat2 far_align_dd = rotate_plane(-(far_angle - p_angle));
+			vec4 far_dd = i_dd * far_scale;
+			far_dd.xy *= far_align_dd;
+			far_dd.zw *= far_align_dd;
+			far_alb = textureGrad(_texture_array_albedo, vec3(far_uv, float(dual_scale_texture)), far_dd.xy, far_dd.zw);
+			far_nrm = textureGrad(_texture_array_normal, vec3(far_uv, float(dual_scale_texture)), far_dd.xy, far_dd.zw);
+			far_alb.rgb *= _texture_color_array[dual_scale_texture].rgb;
+			far_nrm.a = clamp(far_nrm.a + _texture_roughness_mod_array[dual_scale_texture], 0., 1.);
+			// Unpack normal map rotation and blending.
+			far_nrm.xyz = fma(far_nrm.xzy, vec3(2.0), vec3(-1.0));
+			far_nrm.xz *= far_align;
 		}
+
+//INSERT: DUAL_SCALING_MIX
+			// If dual scaling, apply to overlay texture
+			if (id == dual_scale_texture && far_factor > 0.) {
+				alb = mix(alb, far_alb, far_factor);
+				nrm = mix(nrm, far_nrm, far_factor);
+			}
 
 )"
